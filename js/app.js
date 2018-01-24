c9225ef8df3f9968cf210a382ad7c6673d1a83ae
Jack Penton <penton-j@email.ulster.ac.uk>
2018-01-24 03:41:07 +0000

    time to get back to it

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+$('.ani').click(function(e) {
+	$('#mp').addClass('bounceUp');
+	e.preventDefault();
+	setTimeout(function() {
+	  window.location.href = "major-project.html";
+	}, 900);
+	console.log("I got this far");
+});
