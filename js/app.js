1edeb251a56bac8a24d7e7e30c5f837f43380e55
Jack Penton <penton-j@email.ulster.ac.uk>
2018-01-26 18:05:04 +0000

    major changes and updates to case studies and responsiveness

--- js/app.js
+++ js/app.js
@@ -8,3 +8,11 @@ $('.ani').click(function(e) {
 	}, 900);
 	console.log("I got this far");
 });
+$('.visit').click(function(e) {
+	$(this).addClass('rubberBand');
+	e.preventDefault();
+	setTimeout(function() {
+	  window.location.href = "sites/mp";
+	}, 500);
+	console.log("I got this far");
+});
